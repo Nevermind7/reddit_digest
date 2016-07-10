9899d9c60354fe1c6216dd2034147ce041f148e5
Benjamin Eßer <esser@anvo-systems-dresden.com>
2016-07-10 15:40:45 +0200

    Implement sqlite3 database to track user karma

--- digest.py
+++ digest.py
@@ -5,13 +5,14 @@ import time
 import smtplib
 from email.mime.text import MIMEText
 import OAuth2Util
+import sqlite3
 
 class DigestMailer():
     
-    def __init__(self, settings):
-        self.user = settings['user']
+    def __init__(self, user_data):
+        self.user = user_data['username']
         self.name = 'Reddit_Digest'
-        self.address = settings['email']
+        self.address = user_data['mail']
         self.msg = MIMEText('test')
         self.msg['Subject'] = 'Reddit digest for u/{} on {}'.format(self.user, 
                                                                     str(dt.today()).split(' ')[0])
@@ -22,30 +23,51 @@ class DigestMailer():
 
 class Digester():
     
-    def __init__(self, settings, version):
-        self.user_agent = 'digest for reddit v{} by u/individual_throwaway'.format(version)
-        self.r = praw.Reddit(user_agent=self.user_agent)
+    def __init__(self, user_agent, user_data):
+        self.r = praw.Reddit(user_agent=user_agent)
         self.o = OAuth2Util.OAuth2Util(self.r)
+        self.user_data = user_data
+            
+    def get_karma_change(self):
+        self.o.refresh()
+        comment_karma_old = self.user_data['comment_karma']
+        self.comment_karma = self.r.get_me().comment_karma
+        link_karma_old = self.user_data['link_karma']
+        self.link_karma = self.r.get_me().link_karma
+        return (self.comment_karma - comment_karma_old,
+                self.link_karma - link_karma_old)
     
-    def get_karma(self):
-        return self.r.get_me().comment_karma
+    def update_user_db(self):
+        with sqlite3.connect('user_data.db') as conn:
+            c = conn.cursor()
+            c.execute("UPDATE users SET comment_karma=?, link_karma=?\
+                       WHERE username=?", (self.comment_karma, 
+                                           self.link_karma, 
+                                           self.user_data['username']))
     
     def get_subscriptions(self):
-        return list(self.r.get_my_subreddits())
+        self.o.refresh()
+        return [str(x) for x in self.r.get_my_subreddits()]
 
-def load_settings():
-    with open('settings.json', 'r') as _settings:
-        settings = json.load(_settings)
-    return settings
+def load_user_data_from_db():
+    columns = ['username', 'comment_karma', 'link_karma', 'mail']
+    with sqlite3.connect('user_data.db') as conn:
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute("SELECT * FROM users")
+        data = c.fetchone()
+        return {x: data[x] for x in columns}
 
 def main():
-    settings = load_settings()    
-    digest = Digester(settings, __version__)
-    print(digest.get_subscriptions())
-    mail = DigestMailer(settings)
+    user_data = load_user_data_from_db()
+    digest = Digester(USER_AGENT, user_data)
+    print(digest.get_karma_change())
+    digest.update_user_db()
+    mail = DigestMailer(user_data)
     #mail.send()
 
-__version__ = '0.1'
+VERSION = '0.1'
+USER_AGENT = 'digest for reddit v{} by u/individual_throwaway'.format(VERSION)
     
 if __name__ == '__main__':
     main()
