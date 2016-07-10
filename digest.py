d87493bc38592e492f75c959388853f85c9d9cfe
Benjamin Eßer <esser@anvo-systems-dresden.com>
2016-07-10 17:14:33 +0200

    add default.json for default subreddits

--- digest.py
+++ digest.py
@@ -1,11 +1,10 @@
 import praw
-import json
 from datetime import datetime as dt
-import time
 import smtplib
 from email.mime.text import MIMEText
 import OAuth2Util
 import sqlite3
+import json
 
 class DigestMailer():
     
@@ -34,10 +33,11 @@ class Digester():
         self.comment_karma = self.r.get_me().comment_karma
         link_karma_old = self.user_data['link_karma']
         self.link_karma = self.r.get_me().link_karma
+        self._update_user_db()
         return (self.comment_karma - comment_karma_old,
                 self.link_karma - link_karma_old)
     
-    def update_user_db(self):
+    def _update_user_db(self):
         with sqlite3.connect('user_data.db') as conn:
             c = conn.cursor()
             c.execute("UPDATE users SET comment_karma=?, link_karma=?\
@@ -45,9 +45,14 @@ class Digester():
                                            self.link_karma, 
                                            self.user_data['username']))
     
-    def get_subscriptions(self):
+    def get_3_hottest_submissions_last_day(self):
         self.o.refresh()
-        return [str(x) for x in self.r.get_my_subreddits()]
+        subreddits = [str(x) for x in self.r.get_my_subreddits()]
+        for sub in subreddits:
+            sub = self.r.get_subreddit(sub)
+            subs = sub.subscribers
+            top = [str(x) for x in sub.get_hot(time='day', limit=3)]
+            print(sub, subs, top)
 
 def load_user_data_from_db():
     columns = ['username', 'comment_karma', 'link_karma', 'mail']
@@ -58,11 +63,18 @@ def load_user_data_from_db():
         data = c.fetchone()
         return {x: data[x] for x in columns}
 
+def load_default_subs():
+    with open('default.json', 'r') as default:
+        defaults = json.load(default)
+    defaults = [str(x['data']['url']).replace('/r/','').replace('/','') 
+                for x in defaults['data']['children']]
+    return defaults
+
 def main():
     user_data = load_user_data_from_db()
+    default_subs = load_default_subs()
     digest = Digester(USER_AGENT, user_data)
-    print(digest.get_karma_change())
-    digest.update_user_db()
+    digest.get_3_hottest_submissions_last_day()
     mail = DigestMailer(user_data)
     #mail.send()
 
